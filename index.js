80a82cf285260584e083cfce33f928643cb7704d
Andrew Blejde <snehp491@gmail.com>
2022-08-02 23:00:17 -0400

    Move license badge and first pass of writing file out

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 // THEN a high-quality, professional README.md is generated with the
@@ -29,12 +30,23 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`./output/${fileName}`, data, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("File written successfully\n");
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((responses) => {
-        console.log(generateMarkdown(responses));
+        return generateMarkdown(responses);
+    }).then((markdown) => {
+        writeToFile('README.md', markdown);
     });
 }
 
---
+++ output/README.md
@@ -0,0 +1,29 @@
+# Test Project ![badge](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+  ## Description
+  Test project edescription
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Tests](#Tests)
+  * [Contributing](#Contributing)
+  * [Questions](#Questions)
+    
+  ## Installation
+  npm install
+  
+  ## Usage
+  npm run start
+  
+  ## License
+  This project is licensed under the [MIT License](https://choosealicense.com/licenses/mit/) license. 
+  
+  ## Tests 
+  npm run tests
+
+  ## Contributing
+  raise a PR
+  
+  ## Questions
+  Follow on [GitHub](https://github.com/undefined) or via email [snehp491@gmail.com](mailto:snehp491@gmail.com)

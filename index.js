b644ef960d7d698c3576998bbdf75d6f51926f65
Andrew Blejde <snehp491@gmail.com>
2022-08-02 21:57:49 -0400

    initial skeleton for main file

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// THEN a high-quality, professional README.md is generated with the
+// title of my project and sections entitled
+// Description,
+// Table of Contents,
+// Installation,
+// Usage,
+// License,
+// Contributing,
+// Tests,
+// and Questions
+const questions = [
+    {type: 'input', name: 'title'}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log(responses);
+    });
+}
 
 // Function call to initialize app
 init();
